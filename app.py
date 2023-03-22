9a92dd86f31784b464124828f58ad6102aee8ed7
molutyagi1 <128584143+molutyagi1@users.noreply.github.com>
2023-03-22 15:47:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ app.app_context().push()
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
